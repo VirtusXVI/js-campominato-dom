2cbc71e5bc6a7e21ed65b14f948d208ca62ae7cf
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-15 16:17:59 +0200

    completed grid

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // let bombs = gameBombs();
 let button = document.querySelector(".btn");
-// boxGen(userDifficulty);
+let mainGridClass;
 button.addEventListener("click", 
 function(){
     let userDifficulty = userInput();
+    boxGen(userDifficulty);
     // L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
     // con difficoltà 1 => tra 1 e 100
     // con difficoltà 2 => tra 1 e 81
@@ -17,18 +18,62 @@ function(){
             switch(userChoice) {
                 case 1:
                     gameDifficulty = 100;
+                    mainGridClass = "easy";
                     break;
                 case 2:
                     gameDifficulty = 81;
+                    mainGridClass = "hard";
                     break;
                 case 3:
                     gameDifficulty = 49;
+                    mainGridClass = "crazy";
                     break
                 default:
                     gameDifficulty = "errore";
                     throw "inserisci un numero tra 1 e 3!"
             }
+            console.log(gameDifficulty);
             return gameDifficulty;
+        }
+    }
+    function boxGen(userDifficulty){
+        let container = document.querySelector(".ms_box_container");
+        if(userDifficulty === 100){
+            for(let i = 1;i <= userDifficulty; i++){
+                // container.(`<div class="box size easy"><span>${i}</span></div>`);
+                let newCell = document.createElement("div");
+                newCell.innerHTML = `<span>${i}</span>`;
+                newCell.classList.add("box");
+                newCell.classList.add("size");
+                newCell.classList.add("easy");
+                console.log(newCell);
+                console.log(container);
+                container.append(newCell);
+            }
+        } else if(userDifficulty === 81){
+            for(let i = 1;i <= userDifficulty; i++){
+                // container.(`<div class="box size easy"><span>${i}</span></div>`);
+                let newCell = document.createElement("div");
+                newCell.innerHTML = `<span>${i}</span>`;
+                newCell.classList.add("box");
+                newCell.classList.add("size");
+                newCell.classList.add("hard");
+                console.log(newCell);
+                console.log(container);
+                container.append(newCell);
+            }
+        } else if(userDifficulty === 49){
+            for(let i = 1;i <= userDifficulty; i++){
+                // container.(`<div class="box size easy"><span>${i}</span></div>`);
+                let newCell = document.createElement("div");
+                newCell.innerHTML = `<span>${i}</span>`;
+                newCell.classList.add("box");
+                newCell.classList.add("size");
+                newCell.classList.add("crazy");
+                console.log(newCell);
+                console.log(container);
+                container.append(newCell);
+            }
         }
     }
 }
@@ -49,11 +94,3 @@ function gameBombs(){
 // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 // La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-function boxGen(userDifficulty){
-    let container = document.querySelector(".box-container");
-    if(userDifficulty === 100){
-        for(let i = 1;i <= userDifficulty; i++){
-            container.append("")
-        }
-    }
-}
